c799b468531b99a1a2979c4ecf47a37950d0f9e2
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-13 09:34:04 +0800

    [lk] add addition operation at StudentsController

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Controller/StudentsController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Controller/StudentsController.java
@@ -1,8 +1,19 @@
 package com.thoughtworks.capability.gtb.restfulapidesign.Controller;
 
+import com.thoughtworks.capability.gtb.restfulapidesign.Service.StudentsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentsController {
+private StudentsService studentsService;
+public StudentsController(StudentsService studentsService){
+    this.studentsService = studentsService;
+}
 
+@PostMapping(path="/addition")
+    public void addStudents(@RequestBody ){
+
+}
 }
--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Domain/StudentsGroup.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Domain/StudentsGroup.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Groups {
+public class StudentsGroup {
     int groupId;
     String groupName;
     String groupNote;
